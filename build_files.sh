698f265b3807c1ec63b244108b80a153ba8ff70b
Damien Sullins <hi@usertermed.org>
2026-07-26 20:13:56 -0500

    Run inside venv on UV Vercel

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,4 @@
 uv venv .venv --python 3.14
 uv pip install -r requirements.txt
 
-# 2. Run the collection process
-python3 manage.py collectstatic --noinput --clear
+uv run manage.py collectstatic --noinput --clear
